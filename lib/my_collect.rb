3f4147e2bf226e0433c5f59f1d2e1f785fba7078
Renee Nordholm <reneenordholm@gmail.com>
2019-04-02 11:48:18 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+def my_collect(languages)
+  languages do |language|
+    language.upcase
+  end
+end
 
-
+def my_collect(students)
+  names = []
+  students.each do |stu|
+    names << stu.split(" ").first
+  end
+  names
+end
